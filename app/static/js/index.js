b220f9f37c5cf04e77a0f2d4d5a691e0e4aebe48
Rahimakhanim <rehimeagazade@gmail.com>
2024-02-03 21:38:18 +0400

    auth

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -2,10 +2,6 @@ var updateBtns = document.getElementsByClassName('update-cart')
 var infoModal = document.getElementById('info-modal')
 var closeInfoModal = document.getElementById('close-info-modal')
 var info = document.getElementById('info')
-console.log('rnjgkn')
-console.log(infoModal)
-
-
 
 closeInfoModal.addEventListener('click',function(){
     infoModal.style.display = 'none'
@@ -53,42 +49,4 @@ function updateUserOrder(productId,action,page){
 }
 
 
-
-
-// function updateUserOrder(productId,action,page){
-//         // Obtain the CSRF token from the hidden input field
-//         var csrfToken = $('[name="csrfmiddlewaretoken"]').val();
-
-//         // Get the data from the form
-//         var formData = {
-//             'productId': productId,
-//             'action': action
-//         };
-
-//         // Send AJAX request
-//         $.ajax({
-//             type: 'POST',
-//             url: '/update_item/',  // Adjust the URL as per your project structure
-//             data: formData,
-//             headers: {
-//                 'X-CSRFToken': csrfToken
-//             },
-//             success: function (response) {
-//                 if (response.status === 'success') {
-//                     // Clear the form
-//                     // $('#name').val('');
-//                     // $('#age').val('');
-
-//                     // Update the table
-                  
-//                 } else {
-//                     alert('Error adding patient');
-//                 }
-//             },
-//             error: function () {
-//                 alert('Error adding patient');
-//             }
-//         });
-    
-// }
 
