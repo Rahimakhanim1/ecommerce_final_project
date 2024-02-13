b5fed417a406cde6b971d2f77f3cef8cab38e534
Rahimakhanim <rehimeagazade@gmail.com>
2024-02-13 23:54:06 +0400

    shop update

--- app/static/js/shopping-cart.js
+++ app/static/js/shopping-cart.js
@@ -1,10 +1,31 @@
 let updateCart = document.querySelectorAll('.item-quantity')
-$('. updateItemValue').on('click',function(){
-    data = {['id':1,'itemId':12,'value':3]}
-    k=1
-    updateCart.forEach(
-        data.id = k
-    )
+$('.update-item-value').on('click',function(){
+    if (user==='AnonymousUser'){
+        list.map(function(item){
+        item.onclick = function() {
+          var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+          var toastList = toastElList.map(function(toastEl) {
+            return new bootstrap.Toast(toastEl)
+          })
+          toastList.forEach(toast => toast.show()) 
+        }})
+    else{
+    let data = {
+        'items': [
+           
+        ]
+    }   
+    updateCart.forEach( function(item){  
+        data.items.push(
+            {
+                "itemId":item.name,
+                "itemValue":item.value,
+            }
+        )
+       
+     })
+    }
+   
 })
 
-console.log(data)
+
